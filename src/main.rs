3df2b185d27ef2850381dffd4866f6ec11614452
ChitreshN <chitreshnarra@gmail.com>
2023-06-01 23:29:54 +0530

    Add println macros to vga_buffer

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,10 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello World{}",1);
+    println!("humm??");
     loop{}
 }
 
