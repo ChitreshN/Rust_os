95942f1c324a10e5cc5f233080d96660383087da
ChitreshN <chitreshnarra@gmail.com>
2023-06-05 00:02:48 +0530

    Add tests that should fail to testing pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
@@ -9,21 +9,7 @@ mod serial;
 
 use core::panic::PanicInfo;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum Qemuex{
-    Succcess    = 0x10,
-    Failed      = 0x11,
-}
 
-pub fn exit_qemu(exit_code : Qemuex){
-    use x86_64::instructions::port::Port;
-
-    unsafe{
-        let mut port = Port::new(0xf4);// iobase for isa-debug-exit device
-        port.write(exit_code as u32);
-    }
-}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -35,28 +21,7 @@ pub extern "C" fn _start() -> ! {
     loop{}
 }
 
-pub trait Testable{
-    fn run(&self) -> ();
-}
 
-impl<T> Testable for T where T:Fn(),{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[cfg(test)]
-
-fn test_runner(tests: &[&dyn Testable]){
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(Qemuex::Succcess);
-}
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -68,8 +33,5 @@ fn panic(_info: &PanicInfo) -> !{
 #[cfg(test)]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> !{
-    serial_println!("[failed]\n");
-    serial_println!("Error:: {}\n",_info);
-    exit_qemu(Qemuex::Failed);
-    loop{}
+    rust_os::test_panic_handler(_info)
 }
--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,11 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![test_runner(rust_os::test_runner)]
 
 use core::panic::PanicInfo;
-
+use rust_os::println;
 #[no_mangle]
 pub extern "C" fn _start() -> !{
     test_main();
@@ -13,10 +13,15 @@ pub extern "C" fn _start() -> !{
 }
 
 fn test_runner(tests: &[&dyn Fn()]){
-    unimplemented();
+    unimplemented!();
+}
+
+#[test_case]
+fn test_println(){
+    println!("test_println_output");
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> !{
-    loop{}
+    rust_os::test_panic_handler(info)
 }
