51f6fee5c3732141b032d7cc0b27225064f38cf3
ChitreshN <chitreshnarra@gmail.com>
2023-06-13 11:08:27 +0530

    Format code so that its readable

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -9,24 +9,24 @@ use volatile::Volatile;
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
-pub enum Color {
-    Black = 0,
-    Blue = 1,
-    Green = 2,
-    Cyan = 3,
-    Red = 4,
-    Magenta = 5,
-    Brown = 6,
-    LightGray = 7,
-    DarkGray = 8,
-    LightBlue = 9,
-    LightGreen = 10,
-    LightCyan = 11,
-    LightRed = 12,
-    Pink = 13,
-    Yellow = 14,
-    White = 15,
-}// takes 4 bits at max
+pub enum Color {    // takes 4 bits at max
+    Black       = 0,
+    Blue        = 1,
+    Green       = 2,
+    Cyan        = 3,
+    Red         = 4,
+    Magenta     = 5,
+    Brown       = 6,
+    LightGray   = 7,
+    DarkGray    = 8,
+    LightBlue   = 9,
+    LightGreen  = 10,
+    LightCyan   = 11,
+    LightRed    = 12,
+    Pink        = 13,
+    Yellow      = 14,
+    White       = 15,
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(transparent)]
